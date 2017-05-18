82f425d99f91dada48fcf7fc1dc189d261aec7a0
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-05-18 21:46:22 +0200

    Add one more replacement object

--- replace.js
+++ replace.js
@@ -1,11 +1,9 @@
 const shell = require("shelljs");
 
-const repl1 = {
-    target: "Tester",
-    repl: "Tester_repl"
-};
-
-let replacements = [repl1];
+let replacements = [
+    {target: "Tester", repl: "Tester_repl"}, 
+    {target: "ClassThree", repl: "ClassThree_repl"}
+];
 
 shell.find("./src").filter((file) => file.match(/\.ts$/)).forEach((file) => {
     console.log(file);
@@ -16,13 +14,3 @@ shell.find("./src").filter((file) => file.match(/\.ts$/)).forEach((file) => {
         shell.sed("-i", regexp, `$1${replacement.repl}`, file);
     }
 });
-
-/*new webpack.NormalModuleReplacementPlugin(
-    /src\/Tester.ts/,
-    path.join(__dirname, '/src_repl/Tester_repl.ts')
-),
-
-new webpack.NormalModuleReplacementPlugin(
-    /src\/inheritance_chain\/ClassThree.ts/,
-    path.join(__dirname, '/src_repl/inheritance_chain/ClassThree.ts')
-)*/
